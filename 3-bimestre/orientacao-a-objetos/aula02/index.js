4d4918f141bbbe4335e644b52277c25bad434fa0
Edigar Chaves <edigar.almeida@aluno.ce.gov.br>
2026-08-11 16:09:04 +0000

    ok!

---
+++ 3-bimestre/orientacao-a-objetos/aula02/index.js
@@ -0,0 +1,12 @@
+class Funcionarios{
+    constructor(nome, cargo, salario){
+        this.nome = nome
+        this.cargo = cargo
+        this.salario = salario
+    }
+}
+
+const funcionarioED = new Funcionarios("João", "Desenvolvedor", 5000)
+
+
+module.exports = funcionarioED
