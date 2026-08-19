79e9da71f9799d1da4a177c4de86ae0f6abbd298
Edigar Chaves <edigar.almeida@aluno.ce.gov.br>
2026-08-19 10:28:33 +0000

    comitado

--- 3-bimestre/orientacao-a-objetos/projetos/projeto1-sistema-de-biblioteca/index.js
+++ 3-bimestre/orientacao-a-objetos/projetos/projeto1-sistema-de-biblioteca/index.js
@@ -1,26 +1,26 @@
 class Livro {
     constructor(titulo, autor, ano) {
-        this.titulo = titulo
-        this.autor = autor
-        this.ano = ano
-        this.disponivel = true
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.disponivel = true;
     }
 
-        emprestar(){
-            if (this.disponivel) {
-                this.disponivel = false
-            }
+    emprestar() {
+        if (this.disponivel) {
+            this.disponivel = false;
         }
-    
-        devolver(){
-            if(!this.disponivel) {
-                this.disponivel = true
-            }
-        }
+    }
 
-        estaDisponivel(){
-            return this.disponivel
-            }
+    devolver() {
+        if (!this.disponivel) {
+            this.disponivel = true;
+        }
+    }
+
+    estaDisponivel() {
+        return this.disponivel;
+    }
 }
 
-module.exports = Livro
+module.exports = Livro;
