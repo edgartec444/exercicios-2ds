2ce3ad4cf05e6e149f8a1e7eafdd3e602aab3497
Edigar Chaves <edigar.almeida@aluno.ce.gov.br>
2026-08-28 19:18:42 +0000

    commitado

---
+++ 3-bimestre/orientacao-a-objetos/projetos/projeto2-sistema-de-gestao-de-funcionarios/Funcionario.js
@@ -0,0 +1,32 @@
+class Funcionario {
+
+    #salario
+
+    constructor(nome, cargo, salario) {
+        this.nome = nome
+        this.cargo = cargo
+        this.#salario = salario
+
+        Funcionario.quantidade++
+    }
+
+    get salario() {
+        return this.#salario
+    }
+
+    set salario(novoSalario) {
+        if (novoSalario >= 0) {
+            this.#salario = novoSalario
+        }
+    }
+
+    apresentar() {
+        return `${this.nome} - ${this.cargo}`
+    }
+
+    static quantidadeFuncionarios() {
+        return Funcionario.quantidade
+    }
+}
+
+module.exports = Funcionario
